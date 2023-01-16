f67d18cecc570c393e4cb442fc1693bf07143076
馬場義弘 <babayoshihiro@YB-MBP14.local>
2023-01-16 17:48:21 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts");
+
+module.exports = {
+  mode: "development", //デフォルト
+  // devtool: 'source-map',
+  mode: "development",
+  entry: "./src/js/main.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "js/[name]-[contenthash].js",
+    publicPath: "./",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [["@babel/preset-env", { targets: "> 30%, not dead" }]], //セットになっている、targetsでブラウザーを指定できる。
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(css|sass|scss)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: false, //デバックする時にtrueにするとよい。
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg|gif)/,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name]-[contenthash][ext]",
+        },
+        use: [
+          // webpack4までの記述
+          // {
+          //   loader: 'file-loader',
+          //   options: {
+          //     esModule: false,
+          //     name: 'images/',
+          //   },
+          // },
+          {
+            loader: "image-webpack-loader",
+            options: {
+
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+  plugins: [
+    // new RemoveEmptyScriptsPlugin(),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "./css/[name]-[contenthash].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/templates/index.html",
+    }),
+  ],
+};
